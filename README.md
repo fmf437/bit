74c9ecbf202b2e0ea66ed27043afca8af4e0d6e3
Filipe Marques <eagle.software3@gmail.com>
2019-06-08 17:33:15 +0100

    add README and completing compile_bit

---
+++ README.md
@@ -0,0 +1,17 @@
+#### bit program and name is Copyright of Filipe Marques <eagle.software3@gmail.com> and source code of bit is licensed under GNU GPL v3 or later ####
+
+##### To Do list #####
+
+* Compile c++ projects without dependencies, just the STL
+* Compile c projects without dependencies, just the STL
+* Compile in debug mode for integration to gdb c or/and c++ projects without dependencies, just the STL
+* Link the object files to produce the executable program
+* If choosen execute the program
+* Clean and remove the object files
+* Integrate with gdb to debug
+* Build static library
+* Build shared library
+* Compile in debug mode for integration to gdb c or/and c++ projects with dependencies (library: like sfml, ...)
+* Compile c or/and c++ projects with dependencies (library: like sfml, ...)
+* Install / move the program to directory of system (/usr/bin/)
+* Integrate to produce documentation with doxygen
--- compile_bit.sh
+++ compile_bit.sh
@@ -1,13 +1,35 @@
-# Compile in debug mode -g for development
-g++ -c -g -Wall -Isrc/ -std=c++17 -o src/bit.o src/bit.cpp
-g++ -c -g -Wall -Isrc/ -std=c++17 -o src/library.o src/library.cpp
-g++ -c -g -Wall -Isrc/ -std=c++17 -o src/compile.o src/compile.cpp
-g++ -c -g -Wall -Isrc/ -std=c++17 -o src/args.o src/args.cpp
-g++ -c -g -Wall -Isrc/ -std=c++17 -o src/file.o src/file.cpp
-g++ -c -g -Wall -Isrc/ -std=c++17 -o src/main.o src/main.cpp
-g++ -o bit src/bit.o src/library.o src/compile.o src/args.o src/file.o src/main.o -std=c++17 -lstdc++fs
+#!/bin/bash
+# To compile bit
 
-#g++ -o bit bit.cpp library.cpp compile.cpp args.cpp file.cpp main.cpp -std=c++17 -lstdc++fs -I.
+if [[ $1 = "-d" ]]; then
+  # Compile in debug mode -g for development
+  echo "Compile in debug mode -g for development !"
+  echo "1.1 - Compiling the source files ..."
+  g++ -c -g -Wall -Isrc/ -std=c++17 -o src/bit.o src/bit.cpp;
+  g++ -c -g -Wall -Isrc/ -std=c++17 -o src/library.o src/library.cpp;
+  g++ -c -g -Wall -Isrc/ -std=c++17 -o src/compile.o src/compile.cpp;
+  g++ -c -g -Wall -Isrc/ -std=c++17 -o src/args.o src/args.cpp;
+  g++ -c -g -Wall -Isrc/ -std=c++17 -o src/file.o src/file.cpp;
+  g++ -c -g -Wall -Isrc/ -std=c++17 -o src/main.o src/main.cpp;
+  echo "1.2 - Linking the object files to produce bit ..."
+  g++ -o bit src/bit.o src/library.o src/compile.o src/args.o src/file.o src/main.o -std=c++17 -lstdc++fs;
+  echo "2 - Remove the object files *.o !"
+  rm src/*.o;
+elif [[ $1 = "-r" ]]; then
+  # Compile in release mode
+  echo "1 - Compile bit in release mode !"
+  echo "1.1 - Compiling the source files ..."
+  g++ -c -Wall -Isrc/ -std=c++17 -o src/bit.o src/bit.cpp;
+  g++ -c -Wall -Isrc/ -std=c++17 -o src/library.o src/library.cpp;
+  g++ -c -Wall -Isrc/ -std=c++17 -o src/compile.o src/compile.cpp;
+  g++ -c -Wall -Isrc/ -std=c++17 -o src/args.o src/args.cpp;
+  g++ -c -Wall -Isrc/ -std=c++17 -o src/file.o src/file.cpp;
+  g++ -c -Wall -Isrc/ -std=c++17 -o src/main.o src/main.cpp;
+  echo "1.2 - Linking the object files to produce bit ..."
+  g++ -o bit src/bit.o src/library.o src/compile.o src/args.o src/file.o src/main.o -std=c++17 -lstdc++fs;
+  echo "2 - Remove the object files *.o !"
+  rm src/*.o;
+  echo "3 - Moving bit to /usr/local/bin/ !"
+  sudo mv bit /usr/local/bin/
+fi
 
-# Remove the object files *.o
-rm src/*.o
